2fd16bdc77324cddfca67bd3fea75db22e103f9b
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-29 10:14:16 -0300

    Initial Commit

---
+++ Movie Booking/Movie_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_BookingApp.swift
+//  Movie Booking
+//
+//  Created by Alexander Parreira on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
