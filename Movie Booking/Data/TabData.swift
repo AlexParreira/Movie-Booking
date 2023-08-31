a4e486287f203241ae068b04ed85f8465903ebf6
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-31 12:37:21 -0300

    Custom Tab Bar

---
+++ Movie Booking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  Movie Booking
+//
+//  Created by Alexander Parreira on 31/08/23.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable{
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
